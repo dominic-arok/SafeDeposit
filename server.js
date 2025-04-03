508e9f3d8732e82515bbcadc71a6a9f1b79eb324
Dominic Arokiaraj <dominic.arok@outlook.com>
2025-04-03 10:39:56 -0700

    add server file, index file, and environment variables

---
+++ server.js
@@ -0,0 +1,21 @@
+const multer = require("multer")
+const mongoose = require("mongoose")
+
+const express = require("express")
+const app = express()
+
+const upload = multer( { dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URL)
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send("hi")
+})
+
+app.listen(process.env.PORT)
